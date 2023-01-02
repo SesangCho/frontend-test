1558dd2bbb19b05a56fa2bbe962f4aaea1a7738f
Sesang Cho <sesang.cho@moloco.com>
2023-01-02 18:25:11 +0900

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
+          Edits
           {' '}
           <code>src/App.js</code>
           {' '}
